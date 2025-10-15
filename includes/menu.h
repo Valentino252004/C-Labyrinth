60a602596fc504af84f1b6f611592e37d720aae4
Patey Valentin <pateyvalentin@gmail.com>
2025-10-15 08:54:24 +0200

    fix: removed SDL from code

--- includes/menu.h
+++ includes/menu.h
@@ -1,13 +1,19 @@
+#ifndef MENU_H
+#define MENU_H
+
+#include <SDL2/SDL.h>
+
 /** \brief the action the user will perform
  * 
  * \param loaded Specifies if a labyrinth is currently loaded
- * \param exitIndex A pointer that tells the calling function the index for the exit
  * 
  * \return The code of the action
  */
-char* chooseAction(int loaded);
+char *chooseAction(int loaded);
 
 /** \brief Launches the game's menu
  * 
  */
 void startGame();
+
+#endif
--- main.c
+++ main.c
@@ -8,44 +8,18 @@
 
 int main (int argc, char* argv[]) { 
 
-    if (SDL_Init(SDL_INIT_VIDEO) < 0) {
-        printf("Erreur SDL : %s\n", SDL_GetError());
-        return 1;
-    }
-
+    SDL_Init(SDL_INIT_VIDEO);
     SDL_Window *window = SDL_CreateWindow("Labyrinthe", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 800, 600, 0);
-    if (!window) {
-        printf("Erreur fenêtre : %s\n", SDL_GetError());
-        SDL_Quit();
-        return 1;
-    }
-    
     SDL_Renderer *renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
-    if (!renderer) {
-        printf("Erreur renderer : %s\n", SDL_GetError());
-        SDL_DestroyWindow(window);
-        SDL_Quit();
-        return 1;
-    }
-
-    /* // Lancement du menu principal
-    if (startGame(renderer)) {
-        // Si le joueur clique sur "Jouer"
-        game_loop(renderer);
-    } */
+    
+    //setup a random seed;
+    srand(time(NULL));
+    
+    startGame();
 
     SDL_DestroyRenderer(renderer);
     SDL_DestroyWindow(window);
     SDL_Quit();
-
-    printf("passé\n");
-
-    //setup a random seed;
-    srand(time(NULL));
-    //test seed
-    //srand(0);
-    
-    startGame();
 
     return 0; 
 
